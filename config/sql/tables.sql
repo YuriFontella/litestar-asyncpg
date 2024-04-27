322c614962d495b06bec81f8a04cd6770ab6b72a
Yuri Fontella <yurifc4@gmail.com>
2024-04-27 18:56:39 -0300

    sql

--- config/sql/tables.sql
+++ config/sql/tables.sql
@@ -1,35 +1,32 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+create extension if not exists "uuid-ossp";
 
-create table if not exists users
-(
-    id serial primary key,
-    name varchar not null,
-    email varchar not null,
-    password varchar not null,
-    role varchar,
-    status bool default true
+create table if not exists users (
+  id serial primary key,
+  name varchar not null,
+  email varchar not null,
+  password varchar not null,
+  role varchar,
+  status bool default true
 );
 
-CREATE TABLE IF NOT EXISTS players
-(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    "user" VARCHAR NOT NULL,
-    language VARCHAR(255) DEFAULT 'pt-BR'::CHARACTER VARYING,
-    uuid uuid not null unique default uuid_generate_v4(),
-    status BOOL DEFAULT FALSE
+create table if not exists players (
+  id serial primary key,
+  name varchar not null,
+  "user" varchar not null,
+  language varchar(255) default 'pt-br'::character varying,
+  uuid uuid not null unique default uuid_generate_v4(),
+  status bool default false
 );
 
-CREATE TABLE IF NOT EXISTS teams
-(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL UNIQUE,
-    value DECIMAL(15, 2) NOT NULL,
-    player_id INTEGER,
-    protocol INTEGER NOT NULL UNIQUE,
-    date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES players (id) ON DELETE CASCADE
+create table if not exists teams (
+  id serial primary key,
+  name varchar not null unique,
+  value decimal(15, 2) not null,
+  player_id integer,
+  protocol integer not null unique,
+  date timestamp with time zone not null default current_timestamp,
+  constraint fk_player_id foreign key (player_id) references players (id) on delete cascade
 );
 
-create index IF NOT EXISTS users_index_email on users (email asc);
-CREATE INDEX IF NOT EXISTS teams_index_protocol ON teams (protocol ASC);
+create index if not exists users_index_email on users (email asc);
+create index if not exists teams_index_protocol on teams (protocol asc);
