90ec47a5589f3e651de2b764184e30d522cef75e
Yuri Fontella <yurifc4@gmail.com>
2024-05-01 21:55:32 -0300

    plugin

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from litestar.static_files import create_static_files_router
 from config.plugin.asyncpg import asyncpg
 from config.exception.main import app_exception_handler, internal_server_error_handler
 
+from src.channels.main import channels_plugin
 from src.events.main import on_listener
 from src.middlewares.factory import factory
 from src.middlewares.lifespan import on_startup, on_shutdown
@@ -20,7 +21,7 @@ app = Litestar(
         root.router,
         create_static_files_router(path='/public', directories=['public'], send_as_attachment=True)
     ],
-    plugins=[asyncpg],
+    plugins=[asyncpg, channels_plugin],
     cors_config=cors_config,
     csrf_config=csrf_config,
     compression_config=compression_config,
