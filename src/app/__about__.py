0e5ff3b4888747a467ba8812f03fe809fe0424af
Yuri Fontella <yurifc4@gmail.com>
2025-09-08 22:53:23 -0300

    change: add new files

---
+++ src/app/__about__.py
@@ -0,0 +1,5 @@
+"""
+Application metadata
+"""
+
+__version__ = "0.1.0"
--- src/app/__main__.py
+++ src/app/__main__.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.asgi:app", reload=True)
+    uvicorn.run("src.app.asgi:app", reload=True)
