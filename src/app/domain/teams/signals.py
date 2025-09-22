4caef058969c8e8084232c6c2a0c29497debdbc2
Yuri Fontella <yurifc4@gmail.com>
2025-09-22 00:16:51 -0300

    fix: type event listener core

--- src/app/domain/teams/signals.py
+++ src/app/domain/teams/signals.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
 import asyncio
+from typing import TYPE_CHECKING
 
 from litestar.events import listener
+
+if TYPE_CHECKING:
+    from litestar.events import EventListener
 
 
 @listener("messages")
 async def on_message(text: str) -> None:
     await asyncio.sleep(10)
     print(text)
+
+
+# Type annotation for the listener
+on_message: EventListener
